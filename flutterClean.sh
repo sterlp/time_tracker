2584c0f9bcece6d4e9b5004382988584f421ab05
Paul Sterl <paul@sterl.org>
2026-01-17 18:43:44 +0100

    cleanup

---
+++ flutterClean.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "flutter clean ..."
+flutter clean
+echo "Deleting .flutter-plugins ..."
+rm -rf .flutter-plugins
+echo "Deleting .packages ..."
+rm -rf .packages
+echo "Deleting .symlinks ..."
+rm -rf ios/.symlinks/
+echo "Deleting build/ ..."
+rm -rf build/
+echo "Deleting ios/Pods ..."
+rm -rf ios/Pods
+echo "Deleting ios/Podfile* ..."
+rm ios/Podfile*
+echo "Deleting .pub-cache ..."
+rm -rf "${HOME}/.pub-cache/"
+echo "Deleting pubspec.lock ..."
+rm pubspec.lock
+echo "Deleting ios/Runner.xcodeproj/project.xcworkspace ..."
+rm -rf ios/Runner.xcodeproj/project.xcworkspace
+echo "Deleting Library/Caches/CocoaPods ..."
+rm -rf "${HOME}/Library/Caches/CocoaPods"
+echo "Deleting DerivedData ..."
+rm -rf "~/Library/Developer/Xcode/DerivedData"
+echo "Running flutter packages get ..."
+flutter pub get
+flutter packages get
+flutter build appbundle --release
+echo "Done."
